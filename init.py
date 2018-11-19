731bb481281c8de20cc5e38d3ea3989715ef744f
Scott Bullock <scooterbullock@gmail.com>
2018-11-19 17:44:19 -0500

    Beginnings of file system

--- init.py
+++ init.py
@@ -1,11 +1,15 @@
 from colorama import init
 from colorama import Fore, Back, Style
+import os
 
 init()
 
 # COLORAMA USAGE
 # print(Fore.CYAN + 'some blue text')
 # print(Style.RESET_ALL)
+
+os.chdir("data")
+
 #START SCREEN
 print("Luxury Universal [e]Xperience Operating System (version 8.1.1)\n\
 (c) 1998 Luxury Universal [e]Xperience Incorporated. All rights reserved.\n\
@@ -28,11 +32,16 @@ A PRODUCT OF...\n\
 \n\
 By using this program, you acknowledge that any and all thoughts, experiences, or emotions experienced during its use become exclusive Property of LUX. Do you agree to these terms and conditions? [Agree/Disagree]")
 #YOU CANNOT ESCAPE UNTIL YOU AGREE
+
+homeport = "The Network is like a sea extending in all directions, and the computer is your ship. Welcome to your home port, Captain! From here you can plot a course to anywhere on Psuite Island! Where would you like to go?\n \n [North] - Psuite Settings \n [South] - Local Files"
+
+
 agree = 0
 while(agree == 0):
 	startscrn = input("> ")
 	if startscrn.lower() == "agree":
 		print("Thank You!")
+		locale = homeport
 		agree = 1
 	elif startscrn.lower() == "disagree": 
 		print("You must agree to the terms and conditions to use this software.")
@@ -40,5 +49,10 @@ while(agree == 0):
 		print("I do not recognize that command.")
 #GAME GOES HERE
 while(True):
-    i = input("> ")
-    print(i)
+	print(locale)
+	os.chdir("Local Files")
+	with open("index.txt") as f:
+		print(f.read())
+
+	i = input("> ")
+	print(i)
